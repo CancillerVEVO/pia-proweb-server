a569f6225554249e75a0253dbd750789607dc30f
cancillerVEVO <emma.lopez.ojeda@gmail.com>
2023-05-05 14:53:12 -0600

    Project organized by modules

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.sendStatus(200);
-});
+router.use("/movies", require("./movies"));
 
 module.exports = router;
