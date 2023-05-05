2680aa8d3a91ee19e5fb467c2dd9f3a2d08d3ff3
cancillerVEVO <emma.lopez.ojeda@gmail.com>
2023-05-05 16:32:54 -0600

    Error hanlder middleware added

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const errorHandler = require("./middleware/error.handler");
 require("dotenv").config();
 
 const app = express();
@@ -7,6 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api", require("./routes"));
+app.use(errorHandler);
 app.use(express.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 3001;
---
+++ src/middleware/error.handler.js
@@ -0,0 +1,18 @@
+const AppError = require("../utils/AppError");
+
+const errorHandler = (err, req, res, next) => {
+  console.log(err);
+  if (!(err instanceof AppError)) {
+    res.status(500).json({
+      status: "error",
+      message: "Something went wrong",
+    });
+  }
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+};
+
+module.exports = errorHandler;
