92079264ec34b698a224f043da917f7f8769582e
cancillerVEVO <emma.lopez.ojeda@gmail.com>
2023-05-11 01:17:48 -0600

    Register Validator

--- src/auth/auth.middleware.js
+++ src/auth/auth.middleware.js
@@ -1,3 +1,19 @@
 const { body } = require("express-validator");
+const validationErrors = require("../middleware/validation.middleware");
 
-const registerValidator = [];
+const registerValidator = [
+  body("nombre").exists().withMessage("El nombre es requerido"),
+  body("email").exists().withMessage("El email es requerido"),
+  body("contraseña")
+    .isLength({ min: 6 })
+    .withMessage("La contraseña debe tener un mínimo de 6 caracteres."),
+  body("confirmarContraseña").custom((value, { req }) => {
+    if (value !== req.body.password) {
+      throw new Error("Las contraseñas no coinciden.");
+    }
+    return true;
+  }),
+  validationErrors,
+];
+
+module.exports = registerValidator;
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,5 @@ const router = express.Router();
 
 router.use("/movies", require("./movies"));
 router.use("/auth", require("./auth"));
-router.use("/mock", require("./mock"));
 
 module.exports = router;
