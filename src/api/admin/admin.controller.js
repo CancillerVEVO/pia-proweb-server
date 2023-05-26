3db7838e324f5f7f138af602fae2b4399e24d3eb
cancillerVEVO <emma.lopez.ojeda@gmail.com>
2023-05-26 17:36:14 -0600

    Get Users, Comments, Reviews

--- src/api/admin/admin.controller.js
+++ src/api/admin/admin.controller.js
@@ -1,22 +1,26 @@
 const { successResponse } = require("../../handlers/ResponseHandler");
+const { getAllUsers, getAllComments, getAllReviews } = require("./handlers");
 
 const getAllUsersController = async (req, res, next) => {
   try {
-    res.send("getAllUsersController");
+    const users = await getAllUsers();
+    return successResponse(users, "Usuarios obtenidos")(res);
   } catch (error) {
     next(error);
   }
 };
 const getAllReviewsController = async (req, res, next) => {
   try {
-    res.send("getAllReviewsController");
+    const reviews = await getAllReviews();
+    return successResponse(reviews, "Reviews obtenidas")(res);
   } catch (error) {
     next(error);
   }
 };
 const getAllCommentsController = async (req, res, next) => {
   try {
-    res.send("getAllCommentsController");
+    const comments = await getAllComments();
+    return successResponse(comments, "Comentarios obtenidos")(res);
   } catch (error) {
     next(error);
   }
